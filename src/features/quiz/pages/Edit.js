c88cef6aeca9006008bfa67997d9f32f602f6739
duytk <duy.tran@gleads.vn>
2024-08-27 22:15:12 +0700

    update

--- src/features/quiz/pages/Edit.js
+++ src/features/quiz/pages/Edit.js
@@ -2,24 +2,41 @@ import { Button } from '@mantine/core';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import SlidePreview from '../../../components/SlidePreview';
+import { IconChevronLeft, IconPlus } from '@tabler/icons-react';
 
 function Edit(props) {
     const navigate = useNavigate();
 
     return (
-        <div className="h-screen bg-indigo-950 py-4 flex flex-col gap-4">
-            <Button
-                className="self-start"
-                onClick={() => navigate('/')}
-            >{`<- back`}</Button>
-            <div className="flex gap-2 h-full">
-                <div className="bg-slate-300 h-full rounded-lg min-w-52 p-2">
-                    {Array(3)
-                        .fill(null)
-                        .map((item) => (
-                            <SlidePreview className={'mb-4 last:mb-0'} />
-                        ))}
+        <div className="h-screen min-h-0 bg-indigo-950 py-4 flex flex-col gap-4 ">
+            <div className="self-start ">
+                <Button
+                    onClick={() => navigate('/')}
+                    leftSection={<IconChevronLeft size={14} />}
+                >
+                    Back
+                </Button>
+            </div>
+            <div className="flex gap-2 flex-1 overflow-auto">
+                <div className="bg-slate-300 rounded-lg min-w-52 p-2 pb-0 flex flex-col">
+                    <div className="overflow-y-auto">
+                        {Array(4)
+                            .fill(null)
+                            .map((item) => (
+                                <SlidePreview className={'mb-4 last:mb-0'} />
+                            ))}
+                    </div>
+                    <div className="text-center bg-slate-300 py-4 ">
+                        <Button
+                            leftSection={
+                                <IconPlus className="w-4 h-4 min-w-4" />
+                            }
+                        >
+                            Add question
+                        </Button>
+                    </div>
                 </div>
+
                 <div className="bg-slate-400 h-full rounded-lg flex-auto">
                     COL 2
                 </div>
