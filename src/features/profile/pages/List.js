cf0bbf3301b48f384ac844906ba94c4612d334c0
duyTran <duy.tran@gleads.vn>
2024-10-21 17:24:45 +0700

    Add Quizz item

--- src/features/profile/pages/List.js
+++ src/features/profile/pages/List.js
@@ -1,19 +1,79 @@
-import { Input } from '@mantine/core';
+import { ActionIcon, Avatar, Badge, Button, Input } from '@mantine/core';
+import {
+    IconDotsVertical,
+    IconLock,
+    IconPencil,
+    IconSearch,
+    IconWorld,
+} from '@tabler/icons-react';
 import React from 'react';
 
 function List(props) {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-4">
             <div className="lg:col-span-2 lg:col-start-2">
-                <Input placeholder="Search" size="lg" />
+                <Input
+                    placeholder="Search"
+                    size="lg"
+                    leftSection={<IconSearch />}
+                />
                 <div className="my-3 flex flex-col gap-2">
-                    <div className="flex h-32 min-h-32 rounded bg-slate-200 px-3 py-2">
-                        <img
-                            src="https://placehold.co/300x200/EEE/31343C"
-                            className="object-fit h-full w-44"
-                        />
-                        <div>
-                            <p>Title123</p>
+                    <div className="flex h-32 min-h-32 gap-1 rounded bg-white px-3 py-2">
+                        <div className="relative w-44">
+                            <img
+                                src="https://placehold.co/300x200/EEE/31343C"
+                                className="object-fit h-full w-full min-w-28"
+                            />
+                            <Badge
+                                className="absolute bottom-2 right-3 border border-slate-400"
+                                color="gray"
+                                size="sm"
+                            >
+                                5 Questions
+                            </Badge>
+                        </div>
+                        <div className="flex flex-grow flex-col justify-between">
+                            <div className="flex justify-between">
+                                <p className="text-bold text-xl">Title</p>
+                                <div className="flex items-center gap-1">
+                                    <Badge color="red" leftSection={<IconLock className='w-4 h-4' />}>Private</Badge>
+                                    {/* <Badge
+                                        color="teal"
+                                        leftSection={
+                                            <IconWorld className="h-4 w-4" />
+                                        }
+                                    >
+                                        Public
+                                    </Badge> */}
+                                    <ActionIcon variant="subtle" color="black">
+                                        <IconPencil className="text-slate-900" />
+                                    </ActionIcon>
+                                    <ActionIcon variant="subtle" color="black">
+                                        <IconDotsVertical className="text-slate-900" />
+                                    </ActionIcon>
+                                </div>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-center gap-2">
+                                    <Avatar color="grape" radius="xl">
+                                        DT
+                                    </Avatar>
+                                    <p>Duy Tran</p>
+                                </div>
+                                <div className="flex items-center gap-2">
+                                    <p>10 plays</p>
+                                    <Button
+                                        variant="light"
+                                        color="dark"
+                                        className="min-w-fit"
+                                    >
+                                        Host live
+                                    </Button>
+                                    <Button className="min-w-fit">
+                                        Play solo
+                                    </Button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
