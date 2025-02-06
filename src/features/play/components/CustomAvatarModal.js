047e4eea1f86a71aa626bade23e362c1ba281aa7
duytk <duy.tran@gleads.vn>
2025-02-06 21:55:18 +0700

    updating

--- src/features/play/components/CustomAvatarModal.js
+++ src/features/play/components/CustomAvatarModal.js
@@ -3,6 +3,7 @@ import { createAvatar } from '@dicebear/core';
 import {
     ActionIcon,
     Badge,
+    Button,
     Modal,
     SegmentedControl,
     Text,
@@ -11,45 +12,70 @@ import {
 import { useDisclosure } from '@mantine/hooks';
 import { IconDice6Filled, IconMoodHappyFilled } from '@tabler/icons-react';
 import InputField from 'components/form-controls/InputField';
+import { twMerge } from 'tailwind-merge';
 
 function CustomAvatarModal({ children, form, getRandomAvatar }) {
     const [opened, { open, close }] = useDisclosure(false);
+    const { skinColor } = bigSmile.schema.properties;
+    // console.log('cur values: ', form.getValues('avatar'));
 
-    console.log('cur values: ', form.getValues('avatar'));
+    console.log({ bigSmile });
 
     return (
         <>
             <Modal
                 opened={opened}
                 onClose={close}
-                title={<p className="text-bold text-xl">Custom avatar</p>}
                 centered
-                classNames={{
-                    header: 'bg-indigo-900',
-                    body: 'bg-indigo-900',
-                }}
+                withCloseButton={false}
+                padding={0}
+                radius={10}
             >
-                <div className="flex w-fit items-end gap-2 self-center">
-                    <img
-                        src={createAvatar(bigSmile, {
-                            accessoriesProbability: 100,
-                            size: 120,
-                            backgroundColor: ['18181b'],
-                            radius: 10,
-                            ...form.watch('avatar'),
-                        }).toDataUri()}
-                    />
-                    <div className="flex flex-col">
-                        <Tooltip label="Random avatar" position="right">
-                            <ActionIcon
-                                variant="subtle"
-                                color="white"
-                                onClick={getRandomAvatar}
-                            >
-                                <IconDice6Filled />
-                            </ActionIcon>
-                        </Tooltip>
+                <div className="flex flex-col items-center gap-4 bg-indigo-900 px-4 py-3">
+                    <p className="text-xl font-bold text-white">
+                        Custom avatar
+                    </p>
+                    <div className="flex items-end justify-center gap-2">
+                        <img
+                            src={createAvatar(bigSmile, {
+                                accessoriesProbability: 100,
+                                size: 120,
+                                backgroundColor: ['18181b'],
+                                radius: 10,
+                                ...form.watch('avatar'),
+                            }).toDataUri()}
+                        />
+                        <div className="flex flex-col">
+                            <Tooltip label="Random avatar" position="right">
+                                <ActionIcon
+                                    variant="subtle"
+                                    color="white"
+                                    onClick={getRandomAvatar}
+                                >
+                                    <IconDice6Filled />
+                                </ActionIcon>
+                            </Tooltip>
+                        </div>
                     </div>
+                    <div className="self-start w-full text-lg font-bold text-white">
+                        <div className="grid grid-cols-4 gap-2">
+                            <p>Skin:</p>
+                            <div className="col-span-3 flex grow justify-evenly gap-2">
+                                {skinColor.default.map((color) => (
+                                    <div
+                                        className="h-7 min-w-7 rounded-full border-2 border-black/80"
+                                        style={{
+                                            backgroundColor: `#${color}`,
+                                        }}
+                                    ></div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+
+                    <Button fullWidth size="lg" color="teal" onClick={close}>
+                        OK
+                    </Button>
                 </div>
             </Modal>
             <div onClick={open}>{children}</div>
