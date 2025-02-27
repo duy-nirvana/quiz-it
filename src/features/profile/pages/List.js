00774bee7a1c9e3eccdcb12a313e3ec97d32c8a0
duyTran <duytran@gleads.vn>
2025-02-27 16:09:34 +0700

    add no data quiz

--- src/features/profile/pages/List.js
+++ src/features/profile/pages/List.js
@@ -6,6 +6,7 @@ import {
     Button,
     Center,
     Input,
+    LoadingOverlay,
     Pagination,
     SegmentedControl,
 } from '@mantine/core';
@@ -31,13 +32,15 @@ function List(props) {
     const navigate = useNavigate();
     const [quizzes, setQuizzes] = useState([]);
     const { profile } = useSelector((state) => state.personal);
-    const [loading, setLoading] = useState(false);
     const [currentHostQuiz, setCurrentHostQuiz] = useState();
     const [tabValue, setTabValue] = useState('me');
 
+    const [loadingList, setLoadingList] = useState(false);
+    const [loading, setLoading] = useState(false);
+
     const [total, setTotal] = useState(0);
     const [page, setPage] = useState(1);
-    const [limit, setLimit] = useState(1);
+    const [limit, setLimit] = useState(10);
 
     useEffect(() => {
         if (profile) {
@@ -89,18 +92,26 @@ function List(props) {
     };
 
     const getQuizzes = async (query) => {
-        const { data, total, success } = await quizApi.getAll(query);
+        try {
+            setLoadingList(true);
 
-        if (success) {
-            setTotal(total);
-            setQuizzes(data);
+            const { data, total, success } = await quizApi.getAll(query);
+
+            if (success) {
+                setTotal(total);
+                setQuizzes(data);
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoadingList(false);
         }
     };
 
     console.log({ total });
 
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-1 flex-col items-center">
             <div className="w-full max-w-[1000px] lg:basis-full">
                 <Input
                     placeholder="Search"
@@ -143,108 +154,131 @@ function List(props) {
                     ]}
                     className="my-2"
                 />
-                <div className="mb-3 flex flex-col gap-4">
-                    {quizzes.map((quiz) => (
-                        <div
-                            key={quiz.id}
-                            className="flex h-32 min-h-32 gap-1 rounded bg-white px-3 py-2"
-                        >
-                            <div className="relative w-44 flex-shrink-0">
-                                <img
-                                    src={
-                                        quiz.questions.find(
-                                            (ques) => ques.thumbnail
-                                        )?.thumbnail ||
-                                        'https://placehold.co/300x200/EEE/31343C'
-                                    }
-                                    className="h-full w-full rounded-md object-cover object-top"
-                                />
-                                <Badge
-                                    className="absolute bottom-2 right-3 border border-slate-400"
-                                    color="gray"
-                                    size="sm"
-                                >
-                                    {quiz?.questions?.length} Questions
-                                </Badge>
+                <div className="mb-3">
+                    <div className="relative mb-3 flex flex-col gap-3">
+                        {!quizzes.length ? (
+                            <div className="flex items-center justify-center">
+                                <p className="my-2 text-center text-xl text-white">
+                                    Nothing found
+                                </p>
                             </div>
-                            <div className="flex flex-grow flex-col justify-between">
-                                <div className="flex justify-between">
-                                    <p className="text-bold text-xl">
-                                        {quiz?.title ?? 'Title'}
-                                    </p>
-                                    <div className="flex items-center gap-1">
-                                        {quiz?.is_private ? (
-                                            <Badge
-                                                color="red"
-                                                leftSection={
-                                                    <IconLock className="h-4 w-4" />
+                        ) : (
+                            <>
+                                {quizzes.map((quiz) => (
+                                    <div
+                                        key={quiz.id}
+                                        className="flex h-32 min-h-32 gap-1 rounded bg-white px-3 py-2"
+                                    >
+                                        <div className="relative w-44 flex-shrink-0">
+                                            <img
+                                                src={
+                                                    quiz.questions.find(
+                                                        (ques) => ques.thumbnail
+                                                    )?.thumbnail ||
+                                                    'https://placehold.co/300x200/EEE/31343C'
                                                 }
-                                            >
-                                                Private
-                                            </Badge>
-                                        ) : (
+                                                className="h-full w-full rounded-md object-cover object-top"
+                                            />
                                             <Badge
-                                                color="teal"
-                                                leftSection={
-                                                    <IconWorld className="h-4 w-4" />
-                                                }
+                                                className="absolute bottom-2 right-3 border border-slate-400"
+                                                color="gray"
+                                                size="sm"
                                             >
-                                                Public
+                                                {quiz?.questions?.length}{' '}
+                                                Questions
                                             </Badge>
-                                        )}
-                                        {quiz.created_by === profile?._id && (
-                                            <>
-                                                <ActionIcon
-                                                    variant="subtle"
-                                                    color="black"
-                                                    onClick={() =>
-                                                        navigate(
-                                                            `/quiz/${quiz.id}`
-                                                        )
-                                                    }
-                                                >
-                                                    <IconPencil className="text-slate-900" />
-                                                </ActionIcon>
-                                                <ActionIcon
-                                                    variant="subtle"
-                                                    color="black"
-                                                >
-                                                    <IconDotsVertical className="text-slate-900" />
-                                                </ActionIcon>
-                                            </>
-                                        )}
+                                        </div>
+                                        <div className="flex flex-grow flex-col justify-between">
+                                            <div className="flex justify-between">
+                                                <p className="text-bold text-xl">
+                                                    {quiz?.title ?? 'Title'}
+                                                </p>
+                                                <div className="flex items-center gap-1">
+                                                    {quiz?.is_private ? (
+                                                        <Badge
+                                                            color="red"
+                                                            leftSection={
+                                                                <IconLock className="h-4 w-4" />
+                                                            }
+                                                        >
+                                                            Private
+                                                        </Badge>
+                                                    ) : (
+                                                        <Badge
+                                                            color="teal"
+                                                            leftSection={
+                                                                <IconWorld className="h-4 w-4" />
+                                                            }
+                                                        >
+                                                            Public
+                                                        </Badge>
+                                                    )}
+                                                    {quiz.created_by ===
+                                                        profile?._id && (
+                                                        <>
+                                                            <ActionIcon
+                                                                variant="subtle"
+                                                                color="black"
+                                                                onClick={() =>
+                                                                    navigate(
+                                                                        `/quiz/${quiz.id}`
+                                                                    )
+                                                                }
+                                                            >
+                                                                <IconPencil className="text-slate-900" />
+                                                            </ActionIcon>
+                                                            <ActionIcon
+                                                                variant="subtle"
+                                                                color="black"
+                                                            >
+                                                                <IconDotsVertical className="text-slate-900" />
+                                                            </ActionIcon>
+                                                        </>
+                                                    )}
+                                                </div>
+                                            </div>
+                                            <div className="flex items-center justify-between">
+                                                <div className="flex items-center gap-2">
+                                                    <Avatar
+                                                        color="grape"
+                                                        radius="xl"
+                                                    >
+                                                        DT
+                                                    </Avatar>
+                                                    <p>Duy Tran</p>
+                                                </div>
+                                                <div className="flex items-center gap-2">
+                                                    <p>
+                                                        {quiz?.play_count} plays
+                                                    </p>
+                                                    <Button
+                                                        variant="light"
+                                                        color="dark"
+                                                        className="min-w-fit"
+                                                        onClick={() =>
+                                                            handleHostLive(quiz)
+                                                        }
+                                                        loading={
+                                                            currentHostQuiz ===
+                                                                quiz.id &&
+                                                            loading
+                                                        }
+                                                        disabled={loading}
+                                                    >
+                                                        Host live
+                                                    </Button>
+                                                    <Button className="min-w-fit">
+                                                        Play solo
+                                                    </Button>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="flex items-center justify-between">
-                                    <div className="flex items-center gap-2">
-                                        <Avatar color="grape" radius="xl">
-                                            DT
-                                        </Avatar>
-                                        <p>Duy Tran</p>
-                                    </div>
-                                    <div className="flex items-center gap-2">
-                                        <p>{quiz?.play_count} plays</p>
-                                        <Button
-                                            variant="light"
-                                            color="dark"
-                                            className="min-w-fit"
-                                            onClick={() => handleHostLive(quiz)}
-                                            loading={
-                                                currentHostQuiz === quiz.id &&
-                                                loading
-                                            }
-                                            disabled={loading}
-                                        >
-                                            Host live
-                                        </Button>
-                                        <Button className="min-w-fit">
-                                            Play solo
-                                        </Button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+                                ))}
+                            </>
+                        )}
+                    </div>
+
                     <div className="flex justify-end">
                         <Pagination
                             value={page}
